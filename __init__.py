d432335ff2633a5e1843999230a2ff7e5151e90e
Сыроватский Дмитрий <d.syrovatskiy@ispsystem.com>
2018-11-18 01:41:06 +0800

    регистрация

---
+++ __init__.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from flask_simplelogin import SimpleLogin
+import db, server
+
+def create_app():
+#    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+    SimpleLogin(app)
+    app.config.from_mapping(
+        SECRET_KEY='blah',
+        DATABASE=os.path.join('db/sqlite.db'),
+    )
+
+
+    db.init_app(app)
+    app.register_blueprint(server.bp)
+
+    app.add_url_rule('/', endpoint='index')
+    return app
--- server.py
+++ server.py
@@ -1,42 +1,55 @@
 from flask import Flask
+from db import get_db
+from flask import (
+    render_template, flash, request, Blueprint, redirect, url_for )
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask_simplelogin import SimpleLogin
-from flask import render_template
-from flask import request
-from flask import send_from_directory
-import sqlite3
-app = Flask(__name__)
-SimpleLogin(app)
+bp = Blueprint('server', __name__,)
+# app = Flask(__name__)
+# SimpleLogin(app)
 
-@app.route('/')
-def hello_world():
+@bp.route('/')
+def index():
     return render_template('index.html')
 
-@app.route('/reg', methods=['GET', 'POST'])
-def registration():
+@bp.route('/reg', methods=['GET', 'POST'])
+def register():
     if request.method == 'POST':
-        return "ds"
-    else:
-        return send_from_directory(app.static_folder, "templates/registration.html")
-    # conn = sqlite3.connect("db/sqlite.db")
-    # cursor = conn.cursor()
-    # cursor.execute("""INSERT INTO users VALUES (NULL, 'test3', 'pass', 'http://hghghg')""")
-    # conn.commit()
-    return "ok"
+        username = request.form['username']
+        password = request.form['password']
+        repository = request.form['repository']
+        db = get_db()
+        error = None
 
-# def main():
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif not repository:
+            error = 'Password is required.'
+        elif db.execute(
+                'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {0} is already registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password, repository) VALUES (?, ?, ?)',
+                (username, generate_password_hash(password), repository)
+            )
+            db.commit()
+            return redirect(url_for('simplelogin.login'))
+
+        flash(error)
+
+    return render_template("auth/register.html")
 
 
-# if __name__ == "__main__":
-#     main()
+def login_checker(user):
+    """:param user: dict {'username': 'foo', 'password': 'bar'}"""
+    if user.get('username') == 'chuck' and user.get('password') == 'norris':
+       return True  # <--- Allowed
+    return False  # <--- Denied
 
-# def init_db():
-#     db = get_db()
-#     with app.open_resource('schema.sql', mode='r') as f:
-#         db.cursor().executescript(f.read())
-#     db.commit()
-#
-# @app.cli.command('initdb')
-# def initdb_command():
-#     """Initializes the database."""
-#     init_db()
-#     print('Initialized the database.')
+
+SimpleLogin(bp, login_checker=login_checker)
